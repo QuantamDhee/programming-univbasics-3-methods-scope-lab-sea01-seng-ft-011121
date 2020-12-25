3119254eea902b624a2df1d77ecdf1e905c80c1c
Johndhee Ballesteros <johndheeballesteros13@gmail.com>
2020-12-25 04:20:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -17,6 +17,7 @@ def phrase
 end
 
 def all_phrases
+  puts "It's-a me, Mario!"
   puts 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts "It's Dangerous To Go Alone! Take This."
   puts "Its's-a me, Mario!"
